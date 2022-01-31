6136703384e9e9b0fa95a32c57d571d97cb7e2e3
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2022-01-31 17:25:08 +0700

    Add data from JSON

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,44 @@
 <template>
   <div class="profile">
-    <h1>This is an profile page</h1>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <div class="d-flex">
+            <div class="flex-shrink-0">
+              <img
+                src="../assets/profile.jpg"
+                alt="Media"
+                class="img-fluid rounded-pill user-profile"
+              />
+            </div>
+            <div class="flex-grow-1 ms-3 text-start">
+              <h5 class="fw-bold">{{ firstName }} {{ lastName }}</h5>
+              <p>{{ bio }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+
+export default {
+  name: "Profile",
+  data: function () {
+    return {
+      firstName: "Ridha",
+      lastName: "Ahmad Firdaus",
+      bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi numquam ut architecto autem culpa quos voluptatibus nobis nostrum laboriosam nemo.",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.user-profile {
+  width: 80px;
+}
+</style>
